7a51031e2432b7dfd50f10ce0eb192c1111be0cd
Silvano Catinella <silvano.catinella@fimer.com>
2026-06-09 20:10:35 +0200

    Licence text added

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,22 @@
 // Description:
 //
 //
+// License:  LGPL ver 3.0
+//
+// 		This script is a free software; you can redistribute it and/or modify it under the terms	of the GNU
+// 		Lesser General Public License as published by the Free Software Foundation; either version 3.0 of the License,
+// 		or (at your option) any later version. 
+//
+//		For further details please read the full LGPL text file [https://www.gnu.org/licenses/lgpl-3.0.txt].
+// 		You should have received a copy of the GNU General Public License along with this file; if not, write to the 
+//
+//			Free Software Foundation, Inc.,
+//			59 Temple Place, Suite 330,
+//			Boston, MA  02111-1307  USA
+//
+//
+//
+//                                                                                                               cols=128 tab=6
 ------------------------------------------------------------------------------------------------------------------------------*/
 #include <QApplication>
 #include "mainWindow.hpp"
