c341a7e7ec773f4bc9d43c24ef9be25d64bc8612
scatinella <catinella@yahoo.com>
2026-08-07 09:29:44 +0200

    Default banner added

--- share/winstall_postinstall.sh
+++ share/winstall_postinstall.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
+#-------------------------------------------------------------------------------------------------------------------------------
+#                   __     ___      _               _     ___           _ _ _                                
+#                   \ \   / (_)_ __| |_ _   _  __ _| |   / _ \ ___  ___(_) | | ___  ___  ___ ___  _ __   ___ 
+#                    \ \ / /| | '__| __| | | |/ _` | |  | | | / __|/ __| | | |/ _ \/ __|/ __/ _ \| '_ \ / _ \
+#                     \ V / | | |  | |_| |_| | (_| | |  | |_| \__ \ (__| | | | (_) \__ \ (_| (_) | |_) |  __/
+#                      \_/  |_|_|   \__|\__,_|\__,_|_|   \___/|___/\___|_|_|_|\___/|___/\___\___/| .__/ \___|
+#                                                                                                |_|
+#
+# File:   winstall_postinstall.sh
+#
+# Author: Silvano Catinella <catinella@yahoo.com>
+#
+# Description:
+#	This is the post-installation script. 
+#	It creates a symbolic link to the default size icon, and add the link-filename to the added files list.
+#
+#-------------------------------------------------------------------------------------------------------------------------------
 
 ICONFILE="$PREFIX/share/icons/virtualOscilloscope_32x32.png"
 LINKFILE="$PREFIX/share/icons/virtualOscilloscope.png"
